b7584054da827c206c1afde76dfc2ed6421eddea
Jona <s_heitzer@stud.hwr-berlin.de>
2019-06-01 18:40:21 +0200

    Error in server1.erl:15 Bad argument

--- blockchain_server.erl
+++ blockchain_server.erl
@@ -4,8 +4,13 @@
 
 %% Client routines
 mine_block(Data) -> rpc(blockchain_server, {mine_block, Data}).
-get_blocks() -> rpc(get_blocks, {get_blocks}).
+get_blocks()     -> rpc(blockchain_server, {get_blocks}).
 
 %% Callback routines
 init() -> [get_genesis_block()].
+handle({mine_block, Data}, Blockchain) -> io:format("Not implemented.~n").
+handle(get_blocks) -> io:format("not implemented.~n").
+
+%% Function implementations
 get_genesis_block() -> {0, "genesis block", 012876563735}.
+
