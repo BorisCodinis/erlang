568414ec0df3f4dd5f32b1ac4b8d3714ece83e5e
Boris Naefe <boris.naefe@hotmail.de>
2019-06-01 17:41:54 +0200

    testdateien gelöscht

--- tut2.erl
+++
@@ -1,8 +0,0 @@
--module(tut2).
--export([convert/2]).
-
-convert(N, zentimeter) ->
-	N * 2.54;
-
-convert(M, zoll) ->
-	M / 2.54.
--- tut3.erl
+++
@@ -1,8 +0,0 @@
--module(tut3).
--export([convert_lenght/1]).
-
-convert_lenght({cm, X}) ->
-	{inch, X / 2.54};
-
-convert_lenght({inch, Y}) ->
-	{cm, Y * 2.54}.
