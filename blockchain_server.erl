4da3bb9abda817a6947683f86f18b5e73246f436
Jona <s_heitzer@stud.hwr-berlin.de>
2019-06-02 13:11:15 +0200

    No more errors, not shure whether get_blocks() actually returns the blockchain.

--- blockchain_server.erl
+++ blockchain_server.erl
@@ -1,5 +1,5 @@
 -module(blockchain_server).
--export([init/0, mine_block/1, get_blocks/0]).
+-export([init/0, mine_block/1, get_blocks/0, handle/2]).
 -import(server1, [rpc/2]).
 
 %% Client routines
@@ -8,9 +8,10 @@ get_blocks()     -> rpc(blockchain_server, {get_blocks}).
 
 %% Callback routines
 init() -> [get_genesis_block()].
-handle({mine_block, Data}, Blockchain) -> io:format("Not implemented.~n").
-handle(get_blocks) -> io:format("not implemented.~n").
+handle({mine_block, Data}, Blockchain) -> {ok, Blockchain ++ [create_next_block(Data)]};
+handle({get_blocks}, Blockchain) -> {ok, Blockchain}.
 
 %% Function implementations
 get_genesis_block() -> {0, "genesis block", 012876563735}.
+create_next_block(Data) -> {1, Data, 8352936489}.
 
