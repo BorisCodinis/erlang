81f3cbed7b1906cd7621a514c917ac73d5a9e95c
Boris Naefe <boris.naefe@hotmail.de>
2019-06-03 15:23:39 +0200

    merge commit

--- blockchain_server.erl
+++ blockchain_server.erl
@@ -1,6 +1,7 @@
 -module(blockchain_server).
 -export([init/0, mine_block/1, get_blocks/0, handle/2]).
 -import(server1, [rpc/2]).
+-import(hash, [get_hash/1]).
 
 %%
 %% Ein Block der Blockchain wird dargestellt als Tupel der Form:
@@ -24,6 +25,5 @@ handle({mine_block, Data}, Blockchain) -> {ok, Blockchain ++ [create_next_block(
 handle({get_blocks}, Blockchain) -> {{ok, Blockchain}, Blockchain}.
 
 %% Function implementations
-get_genesis_block() -> {0, "0", os:timestamp(), "genesis block", "01287656373543547347346"}.
+get_genesis_block() -> {0, "0", os:timestamp(), "genesis block", hash:get_hash("genesis block")}.
 create_next_block(Data) -> {1, Data, 8352936489}.
-
