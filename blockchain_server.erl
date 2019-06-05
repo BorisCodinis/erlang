7c1b25a8721db0c3c42b29f9747632bb5fdf2d35
Jona <s_heitzer@stud.hwr-berlin.de>
2019-06-05 13:55:49 +0200

    Refactored blockchain_server.erl.

--- blockchain_server.erl
+++ blockchain_server.erl
@@ -14,34 +14,46 @@
 %%	}
 %%
 
-
+%%
 %% Client routines
 mine_block(Data) -> rpc(blockchain_server, {mine_block, Data}).
 get_blocks()     -> rpc(blockchain_server, {get_blocks}).
 
+%%
 %% Callback routines
 init() -> [get_genesis_block()].
 handle({mine_block, Data}, Blockchain) -> {ok, Blockchain ++ generate_next_block(Data, lists:last(Blockchain))};
-handle({get_blocks}, Blockchain) -> {{ok, Blockchain}, Blockchain}.
+handle({get_blocks}, Blockchain) 	   -> {{ok, Blockchain}, Blockchain}.
 
+%%
 %% Function implementations
 get_genesis_block() ->
-	TS = os:timestamp(),
-	{0, "0", TS, "genesis block", calculate_hash_for_block(0, "0", TS, "genesis block")}.
+	Index = 0,
+	PreviousHash = "0",
+	TimeStamp = os:timestamp(),
+	Data = "genesis block",
+	Hash = calculate_hash_for_block(Index, PreviousHash, TimeStamp, Data),
+
+	%% The generated Block
+	{Index, PreviousHash, TimeStamp, Data, Hash}.
 
 %% Erzeuge den nächsten Block.
 %% TODO: Neuen Block auf Validität prüfen
-
 generate_next_block(Data, LastBlock) ->
 	{Index, _, _, _, Hash} = LastBlock,
 	NextIndex = Index + 1,
 	NextPreviousHash = Hash,
 	NextTimeStamp = os:timestamp(),
 	NextData = Data,
-	NextHash = calculate_hash_for_block(NextIndex, Hash, NextTimeStamp, Data), %% calculate_hash_for_block/1 muss noch implementiert werden
+	NextHash = calculate_hash_for_block(NextIndex, Hash, NextTimeStamp, Data),
 	[{NextIndex, NextPreviousHash, NextTimeStamp, NextData, NextHash}].
 
 calculate_hash_for_block(Index, PrevHash, Timestamp, Data) ->
-
-	BlockToBeHashed = integer_to_list(Index) ++ PrevHash ++ integer_to_list(element(1, Timestamp)) ++ integer_to_list(element(2, Timestamp)) ++ integer_to_list(element(3, Timestamp)) ++ Data,
+	BlockToBeHashed = 
+		   integer_to_list(Index) 
+		++ PrevHash 
+		++ integer_to_list(element(1, Timestamp)) 
+		++ integer_to_list(element(2, Timestamp)) 
+		++ integer_to_list(element(3, Timestamp)) 
+		++ Data,
 	hash:get_hash(BlockToBeHashed).
