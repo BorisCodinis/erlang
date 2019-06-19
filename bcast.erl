3ef9d03f86938fdab38f619d4f9c5bf35fdaf858
Jona <s_heitzer@stud.hwr-berlin.de>
2019-06-19 14:50:03 +0200

    Makes broadcasting kind of work.

--- bcast.erl
+++ bcast.erl
@@ -1,27 +1,41 @@
 -module(bcast).
--export([start/1, start/2, add_peers/2]).
+-export([start/1, start/2, add_peers/2, broadcast/2]).
 
 start(Name, Peers) ->
-	register(Name, spawn(fun() -> loop(Name, init(Peers)) end)),
-	{self(), Name}.
+	register(Name, spawn(fun() -> init(Name, Peers) end)),
+	ok.
 
 start(Name) ->
-	register(Name, spawn(fun() -> loop(Name, init([])) end)),
-	{self(), Name}.
+	register(Name, spawn(fun() -> init(Name) end)),
+	ok.
 
-init(Peers) ->
-	Peers.
+init(Name, Peers) ->
+	InitPeers = add_peers(Peers, [{Name, node()}]),
+	broadcast(request_peers, InitPeers),
+	[P ! {response_peers, InitPeers, self()} || P <- InitPeers],
+	loop(Name, InitPeers).
+
+init(Name) ->
+	Peers = add_peers([], [{Name, node()}]),
+	loop(Name, Peers).
 
 loop(Name, Peers) ->
 	receive
-		Any ->
-			io:format("Answer is ~p~n", [Any]),
-			loop(Name, Peers)
+		{request_peers, From} ->
+			From ! {response_peers, Peers, self()},
+			loop(Name, Peers);
+		{response_peers, ResponsePeers, From} ->
+			%% io:write(From),
+			%% io:write(" send peer list.~n"),
+			UniqPeers = add_peers(Peers, ResponsePeers),
+			[io:fwrite("~p: ~p~n", [{self(), node()}, P]) || P <- Peers],
+			loop(Name, UniqPeers)
 	end.
 
-add_peers(Peers, [NewPeer|T]) ->
-	UpdatedPeers = sets:add_element(NewPeer, Peers),
-	add_peers(UpdatedPeers, T);
-add_peers(Peers, []) ->
-	Peers.
+add_peers(Peers, NewPeers) ->
+	UniqPeers = sets:from_list(Peers ++ NewPeers),
+	sets:to_list(UniqPeers).
 
+broadcast(request_peers, Peers) ->
+	[P ! {request_peers, self()} || P <- Peers],
+	ok.
